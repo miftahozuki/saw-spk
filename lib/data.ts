023889704dda59620297a94abff369e263a3bfec
Miftahus Surur Sutowo <62152669+miftahozuki@users.noreply.github.com>
2024-07-28 12:57:29 +0700

    Update data.ts

--- lib/data.ts
+++ lib/data.ts
@@ -2,7 +2,7 @@ import {prisma} from "@/lib/prisma";
 
 export const getalternatifs = async() => {
     try {
-        const alternatif = await prisma.Alternatif.findMany();
+        const alternatif = await prisma.alternatif.findMany();
         return alternatif;
     } catch (error) {
         throw new Error("Failed to fetch alternatif data ");
